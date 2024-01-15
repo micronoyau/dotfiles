54fd5b61ca4c587e24673dcf760cb7e3697f13e2
micronoyau <leo.lf.dk@gmail.com>
2024-01-15 21:39:40 +0100

    Working C LSP binding for nvim, but incoming calls work only on buffered files

--- config/nvim/lua/micronoyau/lsp.lua
+++ config/nvim/lua/micronoyau/lsp.lua
@@ -1,22 +1,16 @@
 local lspconfig = require('lspconfig')
-lspconfig.clangd.setup {}
+-- lspconfig.clangd.setup {}
 
 local s = vim.keymap.set
 
-s("n", "<leader>lh", ":lua vim.lsp.buf.hover()<cr>")
-s("n", "<leader>lgd", ":lua vim.lsp.buf.declaration()<cr>")
+s("n", "<leader>l?", ":lua vim.lsp.buf.hover()<cr>")
+s("n", "<leader>lhh", ":lua vim.lsp.buf.document_highlight()<cr>")
+s("n", "<leader>lhc", ":lua vim.lsp.buf.clear_references()<cr>")
+s("n", "<leader>ld", ":lua vim.lsp.buf.definition()<cr>")
+s("n", "<leader>lx", ":lua vim.lsp.buf.references()<cr>")
+s("n", "<leader>lr", ":lua vim.lsp.buf.rename()<cr>")
+s("n", "<leader>lt", ":lua vim.lsp.buf.type_definition()<cr>")
+s("n", "<leader>li", ":lua vim.lsp.buf.incoming_calls()<cr>")
 
--- s("n", "<leader>lgi", ":lua vim.lsp.buf.implementation()<cr>")
--- nnoremap <buffer> gd <cmd>lua vim.lsp.buf.definition()<cr>
--- nnoremap <buffer> gD <cmd>lua vim.lsp.buf.declaration()<cr>
--- nnoremap <buffer> gi <cmd>lua vim.lsp.buf.implementation()<cr>
--- nnoremap <buffer> go <cmd>lua vim.lsp.buf.type_definition()<cr>
--- nnoremap <buffer> gr <cmd>lua vim.lsp.buf.references()<cr>
--- nnoremap <buffer> <C-k> <cmd>lua vim.lsp.buf.signature_help()<cr>
--- nnoremap <buffer> <F2> <cmd>lua vim.lsp.buf.rename()<cr>
--- nnoremap <buffer> <F4> <cmd>lua vim.lsp.buf.code_action()<cr>
--- xnoremap <buffer> <F4> <cmd>lua vim.lsp.buf.range_code_action()<cr>
---
--- nnoremap <buffer> gl <cmd>lua vim.diagnostic.open_float()<cr>
--- nnoremap <buffer> [d <cmd>lua vim.diagnostic.goto_prev()<cr>
--- nnoremap <buffer> ]d <cmd>lua vim.diagnostic.goto_next()<cr>
+-- s("n", "<leader>lwa", ":lua vim.lsp.buf.add_workspace_folder()<cr>")
+-- s("n", "<leader>li", ":lua vim.lsp.buf.implementation()<cr>")
