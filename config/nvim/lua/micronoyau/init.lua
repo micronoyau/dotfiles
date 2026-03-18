363a826b188a6580639a8811189bd6435633989f
micronoyau <leo.lf.dk@gmail.com>
2026-03-18 13:14:30 +0100

    clean: Cleaned up neovim config

--- config/nvim/lua/micronoyau/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = ' '
-require("micronoyau.options")
-require("micronoyau.lazy")
-require("micronoyau.keymaps")
-require("micronoyau.treesitter")
-require("micronoyau.lsp")
-require("micronoyau.harpoon")
-require("micronoyau.git")
